00faca779cd1ea102cb0f16d205bcff183817096
hacker <hacker@hacker>
2025-10-02 10:57:41 +0200

    teste3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   darkMode: 'class', // Ativa o modo escuro via classe
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "../src/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
     extend: {},
